7ecac6e4251fe714ef722896190a00ef98f91245
johnthedesigner <john@johnthedesigner.com>
2021-01-20 22:44:08 -0500

    Set up initial global sidebar and navbar

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,65 +1,44 @@
-import Head from 'next/head'
 import styles from '../styles/Home.module.css'
+import Logo from '../public/logo-vertical.svg'
+import IconFB from '../public/icon-fb.svg'
+import IconIG from '../public/icon-ig.svg'
 
 export default function Home() {
   return (
-    <div className={styles.container}>
-      <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
-
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
-
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
+    <div className="page">
+      <div className="page__sidebar">
+        <div className="sidebar__top">
+          <Logo/>
+        </div>
+        <div className="sidebar__bottom">
+          <IconFB className="social-icon"/>
+          <IconIG className="social-icon"/>
+        </div>
+      </div>
+      <main className="page__body">
+        <nav className="navbar">
+          <ul className="navbar__list">
+            <li className="navbar__item">
+              <a className="navbar__link navbar__link--active" href="#">Home</a>
+            </li>
+            <li className="navbar__item">
+              <a className="navbar__link" href="#">Membership</a>
+            </li>
+            <li className="navbar__item">
+              <a className="navbar__link" href="#">Events</a>
+            </li>
+            <li className="navbar__item">
+              <a className="navbar__link" href="#">About Us</a>
+            </li>
+            <li className="navbar__item">
+              <a className="navbar__link" href="#">Contact Us</a>
+            </li>
+          </ul>
+        </nav>
+        <div className={styles.home__body}>
+          <p>Lorem ipsum <a href="#">dolor</a> sit amet.</p>
         </div>
       </main>
-
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
-        </a>
-      </footer>
     </div>
   )
 }
