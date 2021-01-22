5d2c419b94a451a88eb22211a7fd8e2add79d7d3
johnthedesigner <john@johnthedesigner.com>
2021-01-21 22:49:26 -0500

    making links in the sidebar work

--- src/components/page.js
+++ src/components/page.js
@@ -1,26 +1,41 @@
 import _ from 'lodash'
 
-import Navbar, {pages} from '../components/navbar.js'
+import Navbar from '../components/navbar'
+import { getUrl } from '../pages'
 import Logo from '../public/logo-vertical.svg'
 import IconFB from '../public/icon-fb.svg'
 import IconIG from '../public/icon-ig.svg'
 
 export default function Page(props) {
-  return (
-    <div className="page">
-      <div className="page__sidebar">
-        <div className="sidebar__top">
-          <Logo/>
+    return (
+        <div className="page">
+            <div className="page__sidebar">
+                <div className="sidebar__top">
+                    <a href={getUrl()} title="Yankee Dahlia Society | Home">
+                        <Logo />
+                    </a>
+                </div>
+                <div className="sidebar__bottom">
+                    <a
+                        href="https://www.facebook.com/YankeeDahliaSociety/"
+                        title="Yankee Dahlia Society on Facebook"
+                        target="_blank"
+                    >
+                        <IconFB className="social-icon" />
+                    </a>
+                    <a
+                        href="https://www.instagram.com/yankeedahliasociety/"
+                        title="Yankee Dahlia Society on Instagram"
+                        target="_blank"
+                    >
+                        <IconIG className="social-icon" />
+                    </a>
+                </div>
+            </div>
+            <main className="page__body">
+                <Navbar currentUrl={props.currentUrl} />
+                {props.children}
+            </main>
         </div>
-        <div className="sidebar__bottom">
-          <IconFB className="social-icon"/>
-          <IconIG className="social-icon"/>
-        </div>
-      </div>
-      <main className="page__body">
-        <Navbar currentUrl={props.currentUrl}/>
-        {props.children}
-      </main>
-    </div>
-  )
+    )
 }
--- src/pages.js
+++ src/pages.js
@@ -1,40 +1,40 @@
 export const pageTitle = (segment) => {
-    let fragment = "Yankee Dahlia Society";
-    return ( segment ? `${fragment} | ${segment}` : fragment )
-  }
+    let fragment = 'Yankee Dahlia Society'
+    return segment ? `${fragment} | ${segment}` : fragment
+}
 
-export const pageUrl = (path) => {
+export const getUrl = (path) => {
     // let base = 'http://yankeedahliasociety.com';
-    let base = process.env.DOMAIN;
-    return ( path ? `${base}${path}` : base )
-  }
-  
+    let base = process.env.DOMAIN
+    return path ? `${base}${path}` : base
+}
+
 const pages = {
     home: {
-      label: "Home",
-      title: pageTitle("Home"),
-      url: pageUrl()
+        label: 'Home',
+        title: pageTitle('Home'),
+        url: getUrl(),
     },
     membership: {
-      label: "Membership",
-      title: pageTitle("Membership"),
-      url: pageUrl("/membership")
+        label: 'Membership',
+        title: pageTitle('Membership'),
+        url: getUrl('/membership'),
     },
     events: {
-      label: "Events",
-      title: pageTitle("Events"),
-      url: pageUrl("/events")
+        label: 'Events',
+        title: pageTitle('Events'),
+        url: getUrl('/events'),
     },
     about: {
-      label: "About Us",
-      title: pageTitle("About Us"),
-      url: pageUrl("/about")
+        label: 'About Us',
+        title: pageTitle('About Us'),
+        url: getUrl('/about'),
     },
     contact: {
-      label: "Contact Us",
-      title: pageTitle("Contact Us"),
-      url: pageUrl("/contact")
-    }
-  }
-  
-  export default pages
+        label: 'Contact Us',
+        title: pageTitle('Contact Us'),
+        url: getUrl('/contact'),
+    },
+}
+
+export default pages
