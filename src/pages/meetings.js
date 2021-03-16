fe279f8be90d1392f96c50c4010c9528b2114405
johnthedesigner <john@johnthedesigner.com>
2021-03-15 22:47:08 -0400

    Remove images from events and restyle event items

--- src/pages/meetings.js
+++ src/pages/meetings.js
@@ -5,18 +5,35 @@ import Hero from '../components/hero'
 import ImageList from '../components/imageList'
 import Bumper from '../components/bumper'
 import pages from '../pages.json'
-import styles from '../styles/Events.module.css'
+
+const Event = (props) => {
+    return (
+        <div className="event">
+            <h4 className="event__date">
+                {props.date} | {props.time}
+            </h4>
+            <h3 className="event__name">{props.name}</h3>
+            <p className="event__description">
+                <b>Description: </b>
+                {props.description}
+            </p>
+            <p className="event__location">
+                <b>Location: </b> {props.location}
+            </p>
+        </div>
+    )
+}
 
 export default function Events() {
     return (
         <Page page={pages.meetings}>
             <Hero title="Meetings & Events" image="/garden.jpg" />
             <Bumper text="Club meetings will typically be held on the 1st Sunday of the month.  During the dahlia blooming season we will hold a few extra events." />
-            <ImageList image="https://via.placeholder.com/600">
-                <h4>May 30, 2021</h4>
-                <h3>Our First Meeting!</h3>
-                <p>
-                    <b>Description: </b>Our very first in person meeting is
+            <Event
+                date="May 30, 2021"
+                time="Time TBD"
+                name="Our First Meeting!"
+                description="Our very first in person meeting is
                     scheduled on Sunday, May 30, 2021 at Maple Brook Farmstead
                     in Sterling, MA. What a better way to spend your memorial
                     weekend than joining us for the first ever in person club
@@ -25,33 +42,24 @@ export default function Events() {
                     tips to make your growing season a success. Come see the
                     farm as we kick off the growing season and meet our growing
                     partners and members. We will come back in October to see
-                    what the farm looks like in full bloom.
-                </p>
-                <p>
-                    <b>Location: </b> Maple Brook Farmstead, 155 Tuttle Road,
-                    Sterling, MA 01564
-                </p>
-            </ImageList>
-            <ImageList image="https://via.placeholder.com/600">
-                <h4>June 6, 2021</h4>
-                <h3>June Meeting</h3>
-                <p>
-                    <b>Description: </b>Agenda TBD
-                </p>
-                <p>
-                    <b>Location: </b> Wright Locke Farm, Winchester, MA
-                </p>
-            </ImageList>
-            <ImageList image="https://via.placeholder.com/600">
-                <h4>July 3, 2021</h4>
-                <h3>Club BBQ & How to stake and water dahlias</h3>
-                <p>
-                    <b>Description: </b>Agenda TBD
-                </p>
-                <p>
-                    <b>Location: </b> Member's Home (TBD)
-                </p>
-            </ImageList>
+                    what the farm looks like in full bloom."
+                location="Maple Brook Farmstead, 155 Tuttle Road,
+                Sterling, MA 01564"
+            />
+            <Event
+                date="June 6, 2021"
+                time="Time TBD"
+                name="June Meeting"
+                description="Agenda TBD"
+                location="Wright Locke Farm, Winchester, MA"
+            />
+            <Event
+                date="July 3, 2021"
+                time="Time TBD"
+                name="Club BBQ & How to stake and water dahlias"
+                description="Agenda TBD"
+                location="Member's Home (TBD)"
+            />
             <Bumper
                 text="Discover more ways to participate in YDS events & more."
                 buttonUrl="/get-involved"
